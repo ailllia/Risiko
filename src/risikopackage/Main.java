372342d5b02b2396396d6e647c2aca7124c0e44d
Maj <Maj@141.57.74.160>
2019-07-18 09:07:33 +0200

    test

--- src/risikopackage/Main.java
+++ src/risikopackage/Main.java
@@ -3,6 +3,6 @@ package risikopackage;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Lasst uns spielen!");
+        System.out.println("Lasst uns spielen! gleich zwei Runden");
     }
 }
