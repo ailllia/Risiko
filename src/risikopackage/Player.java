63460c6e0c9538102db982327d733223e5608fed
GO-sw <niwid@LAPTOP-KEPKMO0Q.Speedport_W_723V_1_46_000>
2019-07-20 18:41:05 +0200

    Funktion continentComplete, damit FreeContinents keine Fehler mehr hat

--- src/risikopackage/Player.java
+++ src/risikopackage/Player.java
@@ -1,10 +1,13 @@
 package risikopackage;
 
+import java.util.List;
+
 public class Player {
     private String color;
     private String playerMission;
     private int playerArmys;
     private int playernew;
+    private List<String> countriesName;
 
     public Player(String pcolor, String mission, int armys, int newarmys) {
         color = pcolor;
@@ -12,6 +15,14 @@ public class Player {
         playerArmys = armys;
         playernew = newarmys;
     }
+    
+	public boolean continentComplete(Continent continent) {
+		if (continent.completeContinent(countriesName)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
     
 	@Override
 	public String toString() {
