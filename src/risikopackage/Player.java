fea3de06502433e3291c4048245a3737831abe22
GO-sw <niwid@LAPTOP-KEPKMO0Q>
2019-08-03 23:53:53 +0200

    Funktion zur Bestimmung zu verteilender Armeen

--- src/risikopackage/Player.java
+++ src/risikopackage/Player.java
@@ -5,20 +5,46 @@ import java.util.List;
 public class Player {
     private String color;
     private String playerMission;
-    private int playerArmys;
-    private int playernew;
-    private List<String> countriesName;
+    private int playerArmies;
+    //private int playernew;
+    private List<String> occupiedCountriesName;
+    private List<Country> occupiedCountries;
 
     public Player(String pcolor) {
         color = pcolor;
     }
 
     public boolean continentComplete(Continent continent) {
-        return (continent.completeContinent(countriesName));
+        return (continent.completeContinent(occupiedCountriesName));
     }
+    
+    public int getNewArmies() {
+		int newArmies = 0;
+		for (Continent continent : Main.continents) {
+			if (continentComplete(continent))
+				newArmies += continent.getBonusArmies();
+		}
+		if (occupiedCountries.size() >= 9)
+			newArmies += (occupiedCountries.size()/3);
+		else
+			newArmies += 2;
+		return newArmies;
+	}
+    
+    public void moveArmies() {
+		int newArmies = playerArmies - occupiedCountries.size();
+		if (newArmies == 0) {
+			// Ausgabe, dass keine Bewegung moeglich ist
+		} else {
+			for (Country country : occupiedCountries) {
+				country.setArmies();
+			}
+			// newArmies koennen jetzt verteilt werden
+		}
+	}
 
     @Override
     public String toString() {
-        return color + playerMission + playerArmys + playernew;
+        return "Farbe: " + color + ", Mission: " + playerMission + ", Anzahl der Armeen: " + playerArmies; // + playernew
     }
 }
