07e37e8da67720b9cabd95ede002762b863aa449
Maj <Maj@majstrageding18.fritz.box>
2019-08-06 23:18:10 +0200

    # MissionTitle als neue Variable hinzugefügt # getMissionTitle # Beschreibung eines bestimmten Titels ausgeben

--- src/risikopackage/FreeCountries.java
+++ src/risikopackage/FreeCountries.java
@@ -3,8 +3,9 @@ package risikopackage;
 public class FreeCountries extends Mission {
     private int numberOfCountries = 11;
 
-    public FreeCountries(String name, String description) {
+    public FreeCountries(String name, String title, String description) {
         missionName = name;
+        missionTitle = title;
         missionDescription = description;
     }
 
