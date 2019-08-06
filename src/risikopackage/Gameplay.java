54744a4face3dbbb7f48b1f93aa672e18903df06
Maj <Maj@majstrageding18.fritz.box>
2019-08-06 12:08:53 +0200

    kann jetzt Farben und Armeen zuteilen

--- src/risikopackage/Gameplay.java
+++ src/risikopackage/Gameplay.java
@@ -1,6 +1,8 @@
 package risikopackage;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -10,12 +12,16 @@ public class Gameplay extends JFrame {
                                 // die nächste funktion aufgerufen wird
 
     public static void initialising() { // Erste Textausgaben, klick auf "weiter" ruft dann spreadCountries auf
-        FieldGUI.textfield.append("Willkommen bei einer Runde Risiko!");
         spreadCountries(Main.playerOne, Main.playerTwo, Main.countries);
+        paintCountries(Main.playerOne, Main.playerTwo, Main.countries);
+    }
+    
+    public static void welcome() { // Erste Textausgaben, klick auf "weiter" ruft dann spreadCountries auf
+        FieldGUI.textfield.append("Willkommen bei einer Runde Risiko!");
     }
 
     private static void spreadCountries(Player playerOne, Player playerTwo, List<Country> countries) {
-        List<Country> countriesCopy = countries;
+        List<Country> countriesCopy = new ArrayList<>(countries);
         Random chance = new Random();
         int j = 13;
 
@@ -27,7 +33,46 @@ public class Gameplay extends JFrame {
         }
 
         for (int i = 0; i < 7; i++) {
-            playerTwo.getCountryNames().add(countriesCopy.get(i).getCountryName());
+            playerTwo.addCountryToList(countriesCopy.get(i).getCountryName());
         }
     }
+    
+    private static void paintCountries(Player playerOne, Player playerTwo, List<Country> countries) 
+    {
+       	Color rgbCodesOne = Player.PlayerColorCode(playerOne);
+       	Color rgbCodesTwo = Player.PlayerColorCode(playerTwo);
+       	for (int i = 0; i < 14; i++)
+       	{
+           	for (int j = 0; j < 7; j++)
+           	{
+           		if (playerOne.getCountryName(j) == countries.get(i).getCountryName())
+           		{
+           			countries.get(i).setColorOfOwner(rgbCodesOne);
+           			countries.get(i).setArmies();
+           		}
+           		else
+           		{
+           			//
+           		}
+           	}
+       	}
+       	for (int i = 0; i < 14; i++)
+       	{
+           	for (int j = 0; j < 7; j++)
+           	{
+           		if (playerTwo.getCountryName(j) == countries.get(i).getCountryName())
+           		{
+           			countries.get(i).setColorOfOwner(rgbCodesTwo);
+           			countries.get(i).setArmies();
+           		}
+           		else
+           		{
+           			//
+           		}
+           	}
+       	}     	
+    }
+    	 
+
+    
 }
