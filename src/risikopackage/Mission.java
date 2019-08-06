0a4d91344facd7fd01d777b9cd663aface45839a
Maj <Maj@majstrageding18.fritz.box>
2019-08-06 23:18:42 +0200

    # MissionTitle als neue Variable hinzugefügt # getMissionTitle # Beschreibung eines bestimmten Titels ausgeben

--- src/risikopackage/Mission.java
+++ src/risikopackage/Mission.java
@@ -4,13 +4,31 @@ import java.util.List;
 
 public abstract class Mission {
 	protected String missionName;
+	protected String missionTitle;
 	protected String missionDescription;
 	
-	public static Mission create(String name, String description, List<Continent> continents) {
+	public static Mission create(String name, String title, String description, List<Continent> continents) {
 		switch (name) {
-		case "Laender befreien" : return new FreeCountries(name, description);
-		case "Kontinente befreien" : return new FreeContinents(name, description, continents);
+		case "Laender befreien" : return new FreeCountries(name, title, description);
+		case "Kontinente befreien" : return new FreeContinents(name, title, description, continents);
 		default : return null;
 		}
+	}
+	
+	public String getMissionTitle() {
+        return this.missionTitle;
+    }
+	
+	public static String getDescription(String title)
+	{
+		String thatMissionDescription = "blubb";
+		for (int i = 0; i < 6; i++)
+		{
+			if (title == Main.missions.get(i).missionTitle)
+			{
+				thatMissionDescription = Main.missions.get(i).missionDescription;
+			}
+		}
+		return thatMissionDescription;
 	}
 }
