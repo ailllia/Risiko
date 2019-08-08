5a971dd27dec9504103bd0915f33f685d54183f7
merleadhara <52801140+merleadhara@users.noreply.github.com>
2019-08-08 09:59:46 +0200

    true if mission complete

--- src/risikopackage/Continent.java
+++ src/risikopackage/Continent.java
@@ -41,4 +41,54 @@ public class Continent {
     public int getBonusArmies() {
         return bonusArmies;
     }
+    
+    // testet, ob bestimmter Kontinent komplett ist
+    public static boolean complete(String cName, Player PlayerNow)
+    {
+		switch (cName) {
+		case "Otea" : 
+			int oteas = 0;
+	        for (int i = 0; i < PlayerNow.numberOfCountries(); i++) {
+	            for (int j = 0; j < Main.continents.get(0).belongingCountries.size(); j++) {
+	            		if (PlayerNow.getCountryName(i).equals(Main.continents.get(0).belongingCountries.get(j)))
+	            		{
+	            			oteas += 1;
+	            		}
+	                }
+	            }
+			if (oteas == 4)
+				return true;
+			else
+				return false;
+		case "Priya" : 
+			int pryas = 0;
+	        for (int i = 0; i < PlayerNow.numberOfCountries(); i++) {
+	            for (int j = 0; j < Main.continents.get(2).belongingCountries.size(); j++) {
+	            		if (PlayerNow.getCountryName(i).equals(Main.continents.get(2).belongingCountries.get(j)))
+	            		{
+	            			pryas += 1;
+	            		}
+	                }
+	            }
+			if (pryas == 5)
+				return true;
+			else
+				return false;
+		case "Solva" : 
+			int solvas = 0;
+	        for (int i = 0; i < PlayerNow.numberOfCountries(); i++) {
+	            for (int j = 0; j < Main.continents.get(2).belongingCountries.size(); j++) {
+	            		if (PlayerNow.getCountryName(i).equals(Main.continents.get(2).belongingCountries.get(j)))
+	            		{
+	            			solvas += 1;
+	            		}
+	                }
+	            }
+			if (solvas == 5)
+				return true;
+			else
+				return false;
+		default : return false;
+		}
+    }
 }
