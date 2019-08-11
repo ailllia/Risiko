5a601c7d26943fe8f9d6474fb3ff3d31a0451d56
ailllia <45641488+ailllia@users.noreply.github.com>
2019-08-11 18:16:40 +0200

    fixed bug

---
+++ Continent.java
@@ -0,0 +1,116 @@
+package risikopackage;
+
+import java.util.List;
+
+public class Continent {
+    private String continentName;
+    private int bonusArmies;
+    private List<String> belongingCountries;
+
+    /**
+     * Creates a new Continent.
+     *
+     * @param name      the name of the country
+     * @param countries the list of countries belonging to the continent
+     */
+    public Continent(String name, List<String> countries) {
+        this.continentName = name;
+        this.belongingCountries = countries;
+        setBonusArmies();
+    }
+
+    /**
+     * Sets bonus armies that come with the continent.
+     *
+     * @see getBonusArmies()
+     */
+    private void setBonusArmies() {
+        if (belongingCountries.size() < 5)
+            bonusArmies = 1;
+        else
+            bonusArmies = 2;
+    }
+
+    /**
+     * Gets the name of the continent.
+     *
+     * @return name of the continent
+     */
+    public String getName() {
+        return continentName;
+    }
+
+    /**
+     * Returns whether a list contains the complete continent.
+     *
+     * @param countries the list of countries that might contain the complete continent
+     * @return true if countries contains the complete continent; false otherwise
+     */
+    public boolean completeContinent(List<String> countries) {
+        int matchingCountries = 0;
+        for (String country : belongingCountries) {
+            for (String land : countries) {
+                if (country.equals(land)) {
+                    matchingCountries++;
+                    break;
+                }
+            }
+        }
+        return (matchingCountries == belongingCountries.size());
+    }
+
+    /**
+     * Gets the number of bonus armies.
+     *
+     * @return the number of bonus armies
+     * @see setBonusArmies()
+     */
+    public int getBonusArmies() {
+        return bonusArmies;
+    }
+
+    /**
+     * Returns true if a continent is owned completely by a player.
+     *
+     * @param cName     the name of the continent
+     * @param PlayerNow the player whose turn it is
+     * @return false by default
+     */
+    public static boolean complete(String cName, Player PlayerNow) {
+        Gameplay gameplayInstance = Gameplay.getInstance();
+        switch (cName) {
+            case "Otea":
+                int oteas = 0;
+                for (int i = 0; i < PlayerNow.numberOfCountries(); i++) {
+                    for (int j = 0; j < gameplayInstance.getContinents().get(0).belongingCountries.size(); j++) {
+                        if (PlayerNow.getCountryName(i).equals(gameplayInstance.getContinents().get(0).belongingCountries.get(j))) {
+                            oteas += 1;
+                        }
+                    }
+                }
+                return oteas == 4;
+            case "Priya":
+                int pryas = 0;
+                for (int i = 0; i < PlayerNow.numberOfCountries(); i++) {
+                    for (int j = 0; j < gameplayInstance.getContinents().get(1).belongingCountries.size(); j++) {
+                        if (PlayerNow.getCountryName(i).equals(gameplayInstance.getContinents().get(2).belongingCountries.get(j))) {
+                            pryas += 1;
+                        }
+                    }
+                }
+                return pryas == 5;
+            case "Solva":
+                int solvas = 0;
+                for (int i = 0; i < PlayerNow.numberOfCountries(); i++) {
+                    for (int j = 0; j < gameplayInstance.getContinents().get(2).belongingCountries.size(); j++) {
+                        if (PlayerNow.getCountryName(i).equals(gameplayInstance.getContinents().get(1).belongingCountries.get(j))) {
+                            solvas += 1;
+                        }
+                    }
+                }
+                return solvas == 5;
+            default:
+                return false;
+        }
+    }
+}
---
+++ Gameplay.java
@@ -0,0 +1,243 @@
+package risikopackage;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Gameplay extends JFrame {
+    // hier werden die spielzuege in einzelnen funktionen aufgerufen
+    private static Gameplay instance;
+    private Player playerOne = new Player(null);
+    private Player playerTwo = new Player(null);
+    private List<Country> countries = new ArrayList<>();
+    private List<Continent> continents = new ArrayList<>();
+    private List<Mission> missions = new ArrayList<>();
+
+    /**
+     * Creates a new Gameplay.
+     */
+    private Gameplay() {
+    }
+
+    /**
+     * Gets the Gameplay. Initialises a new Gameplay if value of instance is null
+     *
+     * @return instance
+     */
+    public static Gameplay getInstance() {
+        if (instance == null) {
+            instance = new Gameplay();
+        }
+        return instance;
+    }
+
+    /**
+     * Initialises the game.
+     */
+    public void initialising() {
+        Random chance = new Random();
+        spreadCountries(chance);
+        paintCountries(playerOne);
+        paintCountries(playerTwo);
+        chooseMission(chance);
+    }
+
+    /**
+     * Welcomes the players to a round of Risk.
+     */
+    public void welcomeText() {
+        FieldGUI.textfield.append("Willkommen bei einer Runde Risiko!\nJedem Spieler wurden 7 Laender zugeteilt." +
+                " Die Anzahl der Armeen steht in der Farbe des Spielers auf dem Land.\nIn den Spielregeln werden" +
+                " die Phasen eines Spielzuges erklaert.\nKlicke auf 'Weiter' um fortzufahren.\n");
+    }
+
+    /**
+     * Informs the player that he has to deploy armies.
+     *
+     * @param player the player whose turn it is
+     */
+    public void deployArmiesText(Player player) {
+        FieldGUI.next.setEnabled(false);
+        FieldGUI.textfield.append("Du bist am Zug, " + player.getColor() + ".\n\n\tVerteile " + player.getNewArmies() +
+                " neue Armeen auf deine Laender.\nKlicke dafuer auf die Zahlen in den Laendern.\nMit Linksklick" +
+                " fuegst du einem Land eine Armee hinzu.\nMit Rechtsklick ziehst du eine verteilte Armee wieder ab.\n");
+    }
+
+    /**
+     * Informs the player that he can attack other countries if he wants.
+     */
+    public void attackphaseText() {
+        FieldGUI.textfield.append("\n\tBefreie Laender, die von deinem Gegner besetzt sind.\nWaehle dazu ein Land aus," +
+                " das in deinem Besitz ist und in dem mindestens 2 Armeen stationiert sind. Waehle danach\n ein gegnerisches" +
+                " Nachbarland aus.\nMit Rechtsklick kannst du deine jeweilige Auswahl aufheben.\nKlicke auf 'Wuerfeln' um" +
+                " den Befreiungsversuch zu starten.\nWenn du nicht weisst, ob du einen Befreiungsversuch starten kannst," +
+                " klicke 'Pruefen'.\nKlicke auf 'Weiter', um zur naechsten Spielphase zu gelangen.\n");
+    }
+
+    /**
+     * Informs the player that an attack is possible.
+     */
+    public void attackPossibleText() {
+        FieldGUI.textfield.append("\nEs ist ein Angriff moeglich. Starte einen Befreiungsversuch, oder klicke auf" +
+                " 'Weiter', um zur naechsten Spielphase zu gelangen.\n");
+    }
+
+    /**
+     * Informs the player that an attack is impossible.
+     */
+    public void attackNotPossibleText() {
+        FieldGUI.textfield.append("\nEs ist kein Angriff moeglich. Klicke auf 'Weiter', um zur naechsten Spielphase zu" +
+                " gelangen.\n");
+    }
+
+    /**
+     * Informs the player that he can redistribute armies if he wants.
+     */
+    public void redistributionText() {
+        FieldGUI.textfield.append("\n\tVersetze deine Armeen.\nZiehe dafuer" +
+                " zuerst mit Rechtsklick mindestens eine Einheit aus einem deiner Laender ab. Mit Linksklick fuegst" +
+                " du\ndie abgezogenen Einheiten einem Land hinzu.\nWenn du nicht weisst, ob du Einheiten verteilen" +
+                " kannst, klicke 'Pruefen'.\nKlicke auf 'Weiter', um deinen Zug zu beenden.\n");
+    }
+
+    /**
+     * Appends a message to the textfield that no armies can be redistributed.
+     * Informs the player that he has no armies to be redistributed.
+     */
+    public void redistributionAbortText() {
+        FieldGUI.textfield.append("\nDu kannst keine Einheiten neuverteilen. Beende deinen Zug, indem du auf 'Weiter' klickst.\n");
+    }
+
+    /**
+     * Appends a message to the textfield that the player can redistribute armies.
+     * Informs the player that he has armies to be redistributed.
+     *
+     * @param player the player whose turn it is
+     */
+    public void redistributionContText(Player player) {
+        FieldGUI.textfield.append("\nDu kannst " + player.getArmiesAvailableToMove() + " Einheit/en neu verteilen.\n");
+    }
+
+    /**
+     * Appends a congratulatory message to the textfield and signifies the end of the game.
+     */
+    public void finishedGameText() {
+        FieldGUI.textfield.append("HERZLICHEN GLUECKWUNSCH! Du hast deine Mission vor deinem Gegner \n" +
+                "erfuellt und damit dieses Spiel gewonnen!");
+    }
+
+    /**
+     * Randomly distributes seven countries to the players
+     *
+     * @param chance the random generator to determine the positions of the seven first countries to be distributed in countries
+     */
+    private void spreadCountries(Random chance) {
+        List<Country> countriesCopy = new ArrayList<>(countries);
+        int j = 13;
+
+        for (int i = 0; i < 7; i++) {
+            int k = chance.nextInt(j);
+            playerOne.addCountryToList(countriesCopy.get(k).getCountryName(), countriesCopy.get(k));
+            countriesCopy.remove(k);
+            j -= 1;
+        }
+
+        for (int i = 0; i < 7; i++) {
+            playerTwo.addCountryToList(countriesCopy.get(i).getCountryName(), countriesCopy.get(i));
+        }
+    }
+    
+    /* Testfall: Spieler 1 hat zu Beginn bereits Kontinent Otea
+    private static void spreadCountries(Player playerOne, Player playerTwo, List<Country> countries, Random chance) {
+        List<Country> countriesCopy = new ArrayList<>(countries);
+        int j = 13;
+        for (int i = 1; i < 8; i++) {
+            int k = i;
+            playerOne.addCountryToList(countries.get(k).getCountryName(), countries.get(k));
+            countriesCopy.remove(k);
+            j -= 1;
+        }
+        for (int i = 0; i < 14; i++) {
+            playerTwo.addCountryToList(countries.get(i).getCountryName(), countries.get(i));
+            if(i == 0) i = 7;
+        }
+    }
+    */
+
+    /**
+     * Sets armies and the owner of countries.
+     */
+    private void paintCountries(Player player) {
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (player.getCountryName(j).equals(countries.get(i).getCountryName())) {
+                    countries.get(i).setColorOfOwnerCode(Player.PlayerColorCode(player));
+                    countries.get(i).setColorOfOwnerString(player.getColor());
+                    countries.get(i).setArmies();
+                }
+            }
+        }
+    }
+
+    /**
+     * Randomly distributes a mission to the two players.
+     *
+     * @param chance the random generator to determine the position of the chosen mission in missions
+     */
+    private void chooseMission(Random chance) {
+        List<Mission> missionsCopy = new ArrayList<>(missions);
+        int j = 5;
+        int k = chance.nextInt(j);
+        playerOne.setPlayerMission(missionsCopy.get(k).getMissionTitle());
+        missionsCopy.remove(k);
+        j -= 1;
+        k = chance.nextInt(j);
+        playerTwo.setPlayerMission(missionsCopy.get(k).getMissionTitle());
+    }
+
+    /**
+     * Gets the first player.
+     *
+     * @return playerOne
+     */
+    public Player getPlayerOne() {
+        return playerOne;
+    }
+
+    /**
+     * Gets the second player.
+     *
+     * @return playerTwo
+     */
+    public Player getPlayerTwo() {
+        return playerTwo;
+    }
+
+    /**
+     * Gets the list containing countries
+     *
+     * @return countries
+     */
+    public List<Country> getCountries() {
+        return countries;
+    }
+
+    /**
+     * Gets the list containing continents
+     *
+     * @return continents
+     */
+    public List<Continent> getContinents() {
+        return continents;
+    }
+
+    /**
+     * Gets the list containing missions
+     *
+     * @return missions
+     */
+    public List<Mission> getMissions() {
+        return missions;
+    }
+}
