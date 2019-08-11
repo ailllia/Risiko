55325a9d7ffa9553028b80beff22d500072fd4d9
Maj <Maj@majstrageding18.fritz.box>
2019-08-11 21:06:43 +0200

    keine Sternchen

--- src/risikopackage/Gameplay.java
+++ src/risikopackage/Gameplay.java
@@ -1,9 +1,11 @@
 package risikopackage;
 
-import javax.swing.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import javax.swing.JFrame;
 
 public class Gameplay extends JFrame {
     // hier werden die spielzuege in einzelnen funktionen aufgerufen
