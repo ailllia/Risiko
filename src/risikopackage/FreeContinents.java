bdb6a06a91211e060898d35d8798a5547d88266c
GO-sw <swantje-wiechmann@t-online.de>
2019-08-11 23:54:49 +0200

    kommentiert

--- src/risikopackage/FreeContinents.java
+++ src/risikopackage/FreeContinents.java
@@ -6,32 +6,17 @@ public class FreeContinents extends Mission {
     private List<Continent> continents;
 
     /**
-     * creates a new mission in this category
+     * Creates a new mission in this category.
      *
-     * @param name        is the category of a mission
-     * @param title       is the title of one particular mission
-     * @param description is the description of that mission
-     * @param continents  is the lists of possible continents to free
+     * @param name the category of a mission
+     * @param title the title of one particular mission
+     * @param description the description of that mission
+     * @param continents the lists of possible continents to free
      */
     public FreeContinents(String name, String title, String description, List<Continent> continents) {
         setMissionName(name);
         setMissionTitle(title);
         setMissionDescription(description);
         this.continents = continents;
-    }
-
-    /**
-     * tests whether a player owns a whole continent
-     *
-     * @param player is the current player
-     * @return true if a continent is owned by the current player
-     */
-    public boolean missionAccomplished(Player player) {
-        int counter = 0;
-        for (Continent continent : continents) {
-            if (player.continentComplete(continent))
-                counter++;
-        }
-        return (counter == continents.size());
     }
 }
